0fed735c8e9dfb402da222dddca80e0c51a45c12
Antoine Gréa <antoine@grea.me>
2019-12-12 09:28:35 +0100

    Small fixes and build tentative

--- .pancake/filters/4_pancake_glossary.py
+++ .pancake/filters/4_pancake_glossary.py
@@ -15,35 +15,34 @@ def eprint(x):
     pp = pprint.PrettyPrinter(indent=2, stream=sys.stderr)
     pp.pprint(x)
 
-def gls(x, title, plural, entry):
-    command= 'Gls' if title else 'gls'
-    if entry:
+def gls(prefix, glsid):
+    command= 'Gls' if glsid.isupper() else 'gls'
+    if prefix == '-':
         command+= 'entry'
-        command+= 'name' if (plural) else 'plural'
+        command+= 'name' if (glsid.endswith('S')) else 'plural'
     else:
-        command+= 'pl' if (plural) else ''
-    return latex_command(command,x)
+        command+= 'pl' if (glsid.endswith('S')) else ''
+    return ilatex(latex_command(command,glsid.lower()))
 
 def pancake_glossary(key, value, format, meta):
     if format == "latex":
-        v = None
+        regex = "<([\+-])([^\)]+)>"
         if key == 'Str':
-            v = value
+            if re.search(regex, value) is not None:
+                return gls(*re.match(regex, value).group(1,2))
         elif key == "Math":
             v = value[1]
-        regex = "<([\+-])([^\)]+)>"
-        if v is not None and re.search(regex, v) is not None:
-            g = []
-            s = list(map(raw,re.split(regex, v)))
-            for m in re.finditer(regex, v):
-                prefix, glsid = m.group(1,2)
-                g.append(gls(glsid.lower(), glsid.isupper(), glsid.endswith('s'), prefix == '-'))
-            for i,j in enumerate(g):
-                s.insert(2*i+1,j)
-            print("[DEBUG]", file = sys.stderr)
-            eprint(value)
-            eprint(s)
-            return s
+            if v is not None and re.search(regex, v) is not None:
+                g = []
+                s = list(map(Str,re.split(regex, v)))
+                for m in re.finditer(regex, v):
+                    g.append(gls(*m.group(1,2)))
+                for i,j in enumerate(g):
+                    s.insert(2*i+1,j)
+                # print("[DEBUG]", file = sys.stderr)
+                # eprint(value)
+                # eprint(s)
+                return s
 
 if __name__ == '__main__':
     toJSONFilter(pancake_glossary)
