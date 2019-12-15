3fbac5f47247f3cc2c64d77cc836a142c9394425
Antoine Gréa <antoine@grea.me>
2019-12-15 19:11:18 +0100

    fixing glossaries

--- .pancake/filters/4_pancake_glossary.py
+++ .pancake/filters/4_pancake_glossary.py
@@ -5,19 +5,13 @@ Pandoc filter to allow for correct cref usage and more.
 """
 
 from utils import *
-import pprint
-import sys
 import re
 
 from pandocfilters import toJSONFilter
 
-def eprint(x):
-    pp = pprint.PrettyPrinter(indent=2, stream=sys.stderr)
-    pp.pprint(x)
-
-def gls(prefix, glsid):
+def gls(entry, glsid):
     command= 'Gls' if glsid.isupper() else 'gls'
-    if prefix == '-':
+    if entry == '-':
         command+= 'entry'
         command+= 'name' if (glsid.endswith('S')) else 'plural'
     else:
@@ -30,19 +24,6 @@ def pancake_glossary(key, value, format, meta):
         if key == 'Str':
             if re.search(regex, value) is not None:
                 return gls(*re.match(regex, value).group(1,2))
-        elif key == "Math":
-            v = value[1]
-            if v is not None and re.search(regex, v) is not None:
-                g = []
-                s = list(map(Str,re.split(regex, v)))
-                for m in re.finditer(regex, v):
-                    g.append(gls(*m.group(1,2)))
-                for i,j in enumerate(g):
-                    s.insert(2*i+1,j)
-                # print("[DEBUG]", file = sys.stderr)
-                # eprint(value)
-                # eprint(s)
-                return s
 
 if __name__ == '__main__':
     toJSONFilter(pancake_glossary)
